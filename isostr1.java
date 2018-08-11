7c472c2e7aa894d0a5051c6380979928779e3420
sushmithanat <41733975+sushmithanat@users.noreply.github.com>
2018-08-11 10:25:55 +0530

    Update and rename isostr2.java to isostr1.java

--- isostr1.java
+++ isostr1.java
@@ -5,7 +5,7 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class isostr2
+class isostr1
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
@@ -32,10 +32,10 @@ class isostr2
 		if(count1>0 && count2>0)
 		{
 			if(count1==count2)
-			System.out.println("yes");
+			System.out.println("no");
 		}
 		else
-			System.out.println("no");
+			System.out.println("yes");
 			
 	}
 }
