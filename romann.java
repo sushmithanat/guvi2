5744d5b1b650e6425ce6cd00664b63503234b63f
sushmithanat <41733975+sushmithanat@users.noreply.github.com>
2018-08-13 09:37:58 +0530

    Create romann.java

---
+++ romann.java
@@ -0,0 +1,58 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class romann
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		switch(n)
+		{
+			case 1:System.out.println("I");
+				break;
+			case 2:System.out.println("II");
+				break;
+			case 3:System.out.println("III");
+				break;
+			case 4:System.out.println("IV");
+				break;
+			case 5:System.out.println("V");
+				break;
+			case 6:System.out.println("VI");
+				break;
+			case 7:System.out.println("VII");
+				break;
+			case 8:System.out.println("VIII");
+				break;
+			case 9:System.out.println("IX");
+				break;
+			case 10:System.out.println("X");
+				break;
+			case 11:System.out.println("XI");
+				break;
+			case 12:System.out.println("XII");
+				break;
+			case 13:System.out.println("XIII");
+				break;
+			case 14:System.out.println("XIV");
+				break;
+			case 15:System.out.println("XV");
+				break;
+			case 16:System.out.println("XVI");
+				break;
+			case 17:System.out.println("XVII");
+				break;
+			case 18:System.out.println("XVIII");
+				break;
+			case 19:System.out.println("XIX");
+				break;
+			case 20:System.out.println("XX");
+				break;
+		}
+	}
+}
