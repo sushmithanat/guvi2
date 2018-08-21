134bcd0b0658ad6e4862633074349c9790bb3e36
sushmithanat <41733975+sushmithanat@users.noreply.github.com>
2018-08-21 14:53:14 +0530

    Create strap.java

---
+++ strap.java
@@ -0,0 +1,17 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class strap
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		String str=sc.nextLine();
+		str +=" Answer";
+		System.out.println(str);
+	}
+}
