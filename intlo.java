ea39fb66c74106524e687ffe56bf882bca56e117
sushmithanat <41733975+sushmithanat@users.noreply.github.com>
2018-08-28 11:07:05 +0530

    Update and rename intl.java to intlo.java

--- intlo.java
+++ intlo.java
@@ -5,7 +5,7 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class intl
+class intlo
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
@@ -14,6 +14,6 @@ class intl
 		if(n>=-2147483648 && n<=2147483647)
 		System.out.print("INT");
 		else
-		System.out.print("LONG");
+		System.out.print("LONG LONG");
 	}
 }
