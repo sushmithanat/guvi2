8b259f8ea7f37e604f064cb9ffa08202c71e4171
sushmithanat <41733975+sushmithanat@users.noreply.github.com>
2018-08-28 10:42:51 +0530

    Create inter.java

---
+++ inter.java
@@ -0,0 +1,22 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class inter
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int a1=sc.nextInt();
+		int a2=sc.nextInt();
+		int a3=sc.nextInt();
+		int sum=a1+a2+a3;
+		if(sum==180)
+		System.out.print("yes");
+		else
+		System.out.print("no");
+	}
+}
