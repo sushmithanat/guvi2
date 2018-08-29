78ced84ee917c279d87719de11717622b4e5f71b
sushmithanat <41733975+sushmithanat@users.noreply.github.com>
2018-08-29 10:25:47 +0530

    Create divodd.java

---
+++ divodd.java
@@ -0,0 +1,27 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class divodd
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		for(int i=1;i<=n;i++)
+		{
+			if(n%i==0)
+			{
+				int div=n/i;
+				if(div%2!=0)
+				{
+					System.out.print(i);
+					break;
+				}
+			}
+		}
+	}
+}
