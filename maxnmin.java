a4200413f1ed0b0c1d2014539875da4ba852e4e9
sushmithanat <41733975+sushmithanat@users.noreply.github.com>
2018-08-31 16:25:18 +0530

    Create maxnmin.java

---
+++ maxnmin.java
@@ -0,0 +1,55 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class maxnmin
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int[] a=new int[n];
+		int max=a[0],min=a[0],maxi=0,mini=0;
+		int[] b=new int[n];
+		for(int i=0;i<n;i++)
+		a[i]=sc.nextInt();
+		for(int i=0;i<n;i++)
+		b[i]=a[i];
+		for(int i=0;i<n;i++)
+		{
+			for(int j=i+1;j<n;j++)
+			{
+				if(b[i]>b[j])
+				{
+					int tmp=b[i];
+					b[i]=b[j];
+					b[j]=tmp;
+				}
+			}
+		}
+		min=b[0];
+		max=b[n-1];
+		for(int i=0;i<n;i++)
+		{
+			if(a[i]==min)
+			{
+			mini=a[i];
+			break;
+			}
+		}
+		for(int i=0;i<n;i++)
+		{
+			if(a[i]==max)
+			{
+			maxi=a[i];
+			break;
+			}
+		}
+		int res=maxi-mini;
+		System.out.print(res);
+		
+	}
+}
