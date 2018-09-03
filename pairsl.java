0f0b2cfa8d1993f8e388d1e68f9261133b388289
sushmithanat <41733975+sushmithanat@users.noreply.github.com>
2018-09-03 17:04:00 +0530

    Create pairsl.java

---
+++ pairsl.java
@@ -0,0 +1,29 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class pairsl
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int[] a=new int[n];
+		for(int i=0;i<n;i++)
+		a[i]=sc.nextInt();
+		int count=0;
+		for(int i=0;i<n;i++)
+		{
+			for(int j=i+1;j<n;j++)
+			{
+				if(a[i]<a[j])
+				count++;
+			}
+		}
+		System.out.print(count);
+	}
+}
