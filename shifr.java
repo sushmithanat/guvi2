94d1063c9a2ea80af859a871c84e929b0a10d160
sushmithanat <41733975+sushmithanat@users.noreply.github.com>
2018-09-04 23:08:27 +0530

    Update and rename rightk.java to shifr.java

--- shifr.java
+++ shifr.java
@@ -5,14 +5,14 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class rightk
+class shifr
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner sc=new Scanner(System.in);
 		int n1=sc.nextInt();
 		int n2=sc.nextInt();
-		int bit=n1>>n2;
-		System.out.print(bit);
+		double bit=n1>>n2;
+		System.out.print(bit+"0");
 	}
 }
