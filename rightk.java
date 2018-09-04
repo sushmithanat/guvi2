717841fe1d4dce4f638c1eeeb06d065646f93f09
sushmithanat <41733975+sushmithanat@users.noreply.github.com>
2018-09-04 23:05:20 +0530

    Create rightk.java

---
+++ rightk.java
@@ -0,0 +1,18 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class rightk
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n1=sc.nextInt();
+		int n2=sc.nextInt();
+		int bit=n1>>n2;
+		System.out.print(bit);
+	}
+}
