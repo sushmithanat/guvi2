8f9dfb102359e1bd64e98fa71d823dbdeb1e1cd8
sushmithanat <41733975+sushmithanat@users.noreply.github.com>
2018-09-17 13:02:59 +0530

    Create subarf.java

---
+++ subarf.java
@@ -0,0 +1,21 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class subarf
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int[] a=new int[n];
+		for(int i=0;i<n;i++)
+		a[i]=sc.nextInt();
+		int n1=n*(n+1);
+		n1 /= 2;
+		System.out.print(n1);
+	}
+}
