2db8b5c8f51acbb585b24fae9722188f9b19ef65
sushmithanat <41733975+sushmithanat@users.noreply.github.com>
2018-09-20 15:34:56 +0530

    Create kunam.java

---
+++ kunam.java
@@ -0,0 +1,33 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class kunam
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int k=sc.nextInt();
+		int[] a=new int[n];
+		for(int i=0;i<n;i++)
+		a[i]=sc.nextInt();
+		int count=0;
+		for(int i=0;i<n;i++)
+		{
+			if(a[i]==k)
+			count++;
+		}
+		if(count==0)
+		System.out.print("no");
+		else
+		{
+		System.out.print("yes");
+		System.out.print(" ");
+		System.out.print(count);
+		}
+	}
+}
