6d6ee0ef02770308de82407734583802138dca6b
sushmithanat <41733975+sushmithanat@users.noreply.github.com>
2018-10-18 11:02:59 +0530

    Create greatn.java

---
+++ greatn.java
@@ -0,0 +1,34 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class greatn
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int[] a=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		int min=0,j=0;
+		for(int i=0;i<n;i++)
+		{
+			for(j=0;j<n;j++)
+			{
+				if(a[j]%a[i]>=1)
+				break;
+			}
+			if(j==n)
+			{
+			System.out.print(a[i]);
+			break;
+			}
+		}
+	}
+}
