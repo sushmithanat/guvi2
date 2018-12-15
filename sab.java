e3c7b5e7d4be628a551a1f35eacbc0b1b1f0529b
sushmithanat <41733975+sushmithanat@users.noreply.github.com>
2018-12-15 15:53:48 +0530

    Update and rename alar.java to sab.java

--- sab.java
+++ sab.java
@@ -5,7 +5,7 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class alar
+class sab
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
@@ -16,7 +16,7 @@ class alar
 		{
 			if(str.charAt(i)=='a' && str.charAt(i+1)=='b')
 			{
-				cnt += 2;
+				cnt += 1;
 			}
 		}
 		if(cnt>=0)
