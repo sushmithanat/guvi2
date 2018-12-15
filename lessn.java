dcdbbd7111fedf17397c548571c39cb4af82beac
sushmithanat <41733975+sushmithanat@users.noreply.github.com>
2018-12-15 15:17:57 +0530

    Create lessn.java

---
+++ lessn.java
@@ -0,0 +1,28 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class lessn
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int cnt=0;
+		for(int i=2;i<n;i++)
+		{
+			if(n%i==0)
+			{
+				cnt=1;
+				break;
+			}
+		}
+		if(cnt==0)
+		System.out.print("no");
+		else
+		System.out.print("yes");
+	}
+}
