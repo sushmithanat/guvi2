531e668b30c2529de7e2e9c3f0a226ef52cbef00
sushmithanat <41733975+sushmithanat@users.noreply.github.com>
2018-12-15 15:20:20 +0530

    Update lessn.java

--- lessn.java
+++ lessn.java
@@ -5,14 +5,14 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class lessn
+class Ideone
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner sc=new Scanner(System.in);
 		int n=sc.nextInt();
 		int cnt=0;
-		for(int i=2;i<n;i++)
+		for(int i=1;i<=n;i++)
 		{
 			if(n%i==0)
 			{
