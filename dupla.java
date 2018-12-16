93446a5981e16771c6eea6909a7786552c46b0cc
sushmithanat <41733975+sushmithanat@users.noreply.github.com>
2018-12-16 11:56:37 +0530

    Update and rename rem1.java to dupla.java

--- dupla.java
+++ dupla.java
@@ -5,7 +5,7 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class rem1
+class dupla
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
@@ -21,6 +21,8 @@ class rem1
 			if(a[i]!=a[i+1])
 			tmp[j++]=a[i];
 		}
+		if(a[n-2]!=a[n-1])
+		tmp[j++]=a[n-1];
 		int[] b=new int[j];
 		for(int i=0;i<j;i++)
 		b[i]=tmp[i];
