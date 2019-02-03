a757ba0be3c7c5015ad61c0090377a21b81aada6
sushmithanat <41733975+sushmithanat@users.noreply.github.com>
2019-02-03 14:25:32 +0530

    Create rma.java

---
+++ rma.java
@@ -0,0 +1,33 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class rma
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int[] a=new int[n];
+		for(int i=0;i<n;i++)
+		a[i]=sc.nextInt();
+		int tmp=0;
+		Arrays.sort(a);
+		for(int i=0;i<n;i++)
+		{
+			for(int j=i+1;j<n;j++)
+			{
+				if(a[i]==a[j])
+				{
+					
+				}
+				else
+				tmp=a[j];
+			}
+		}
+		System.out.print(tmp);
+	}
+}
