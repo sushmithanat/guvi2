a6ee1812d58fe59a26807edcddf6b47af282235b
sushmithanat <41733975+sushmithanat@users.noreply.github.com>
2019-02-04 14:34:08 +0530

    Create rmspac.java

---
+++ rmspac.java
@@ -0,0 +1,24 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class rmspac
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		String str=sc.nextLine();
+		String st="";
+		for(int i=0;i<str.length()-1;i++)
+		{
+			if(str.charAt(i)==' ' && str.charAt(i+1)!=' ' || str.charAt(i)>='a' && str.charAt(i)<='z')
+			st += str.charAt(i);
+		}
+		st += str.charAt(str.length()-1);
+		System.out.print(st);
+	}
+}
