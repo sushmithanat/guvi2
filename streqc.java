de1997735ed61a1c526ef3cc95bb6738dd51173f
sushmithanat <41733975+sushmithanat@users.noreply.github.com>
2019-02-04 13:21:32 +0530

    Create streqc.java

---
+++ streqc.java
@@ -0,0 +1,38 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class streqc
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		String[] a=new String[n];
+		for(int i=0;i<n;i++)
+		a[i]=sc.next();
+		int cnt=0;
+		Arrays.sort(a);
+		for(int i=0;i<n-1;i++)
+		{
+			if(a[i].equals(a[i+1]))
+			{
+				cnt=1;
+				break;
+			}
+		}
+		if(cnt==0)
+		{
+			if(a[n-1].equals(a[n-2]))
+			cnt=1;
+		}
+		if(cnt==1)
+		System.out.print("yes");
+		else
+		System.out.print("no");
+	}
+}
